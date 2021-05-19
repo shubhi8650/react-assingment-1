9079746b0454ef723475c23b067de2c29fe96c5b
shubhi8650 <soni.shubhamknmiet@gmail.com>
2021-05-19 19:07:50 +0530

    removed the div

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div  id ="root" className="App">
+    
       <p>I am learning React. My life is getting better. </p>
-    </div>
+   
   );
 }
 
