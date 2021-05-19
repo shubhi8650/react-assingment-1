54dbf890863fa47140cef0846bf3b8c899473ab3
shubhi8650 <soni.shubhamknmiet@gmail.com>
2021-05-19 18:36:11 +0530

    modified p tag

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div  id ="root" className="App">
-      <p>"I am learning React. My life is getting better. "</p>
+      <p>I am learning React. My life is getting better. </p>
     </div>
   );
 }
